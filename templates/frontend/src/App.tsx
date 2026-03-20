bd9ae552c78eeaa362d96fb72148f0c14f58f610
akdevsaha-dev <akdevsaha.dev@gmail.com>
2026-03-20 22:26:19 +0530

    implement minimal frontend template

---
+++ templates/frontend/src/App.tsx
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import hero from "./assets/hero.png";
+import go from "./assets/go.png";
+import { Plus } from "lucide-react";
+function App() {
+  const [message, setMessage] = useState<string | null>("");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await axios.get("http://localhost:8000/");
+        console.log(res.data);
+        setMessage(res.data.message);
+      } catch (err) {
+        console.error(err);
+        setMessage("Error fetching data.");
+      }
+    };
+    fetchData();
+  }, []);
+  return (
+    <div id="hero">
+      <div className="flex-center">
+        <img src={go} alt="go" width={200} />
+        <Plus className="mr-8" />
+        <img src={hero} alt="react" width={200} />
+      </div>
+      <div className="mt-10 text-4xl">{message}</div>
+    </div>
+  );
+}
+
+export default App;
